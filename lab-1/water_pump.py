d9b908b2ba0026cc0b44d121b9128a45fd84e726
aekrops <svyatoslavtrostynski24@gmail.com>
2020-10-26 13:08:42 +0200

    added heapsort

--- lab-1/water_pump.py
+++ lab-1/water_pump.py
@@ -27,6 +27,35 @@ class Sort:
 
     @staticmethod
     def heap_sort(list_of_pumps):
-        """ sorting pumps by volume"""
-        pass
+        """ sorting pumps by volume """
+        length = len(list_of_pumps)
 
+        for i in range(length // 2 - 1, -1):
+            Sort.heapify(list_of_pumps, length, i)
+
+        for i in range(length - 1, 0, -1):
+            Sort.swap(i, 0, list_of_pumps)
+            Sort.heapify(list_of_pumps, i, 0)
+        return list_of_pumps
+
+    @staticmethod
+    def swap(elem_index1: int, elem_index2: int, list_of_objects: list):
+        temp = list_of_objects[elem_index1]
+        list_of_objects[elem_index1] = list_of_objects[elem_index2]
+        list_of_objects[elem_index2] = temp
+
+    @staticmethod
+    def heapify(list_of_pumps, length, root):
+        max_value = root
+        left_child = 2 * root + 1
+        right_child = 2 * root + 2
+
+        if left_child < length and list_of_pumps[max_value].liter_per_hour < list_of_pumps[left_child].liter_per_hour:
+            max_value = left_child
+
+        if right_child < length and list_of_pumps[max_value].liter_per_hour < list_of_pumps[right_child].liter_per_hour:
+            max_value = right_child
+
+        if max_value != root:
+            Sort.swap(root, max_value, list_of_pumps)
+            Sort.heapify(list_of_pumps, length, max_value)
