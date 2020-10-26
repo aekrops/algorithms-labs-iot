afec47e6d4ad2474644cf98ff62aaabc8ba0fe2b
aekrops <svyatoslavtrostynski24@gmail.com>
2020-10-26 16:08:09 +0200

    added Analysis of algorithm

---
+++ lab1/algorithm_analysis.py
@@ -0,0 +1,26 @@
+import timeit
+
+
+class AlgoAnalysis:
+
+    # counts amount of comparisons
+    insertion_sort_comparisons_count, heapsort_comparisons_count = 0, 0
+
+    # counts amount of swaps
+    insertion_sort_swaps_count, heapsort_swaps_count = 0, 0
+
+    @staticmethod
+    def algorithm_time(algorithm_name: str):
+        setup = "from __main__ import " + algorithm_name
+        return timeit.timeit(algorithm_name, setup)
+
+    @staticmethod
+    def result_view(algorithm_name: str, sorted_list):
+        print("Name: " + algorithm_name.capitalize())
+        print("Time: " + str(AlgoAnalysis.algorithm_time(algorithm_name)))
+        if 'insertion' in algorithm_name:
+            print("Amount of comparison operations: " + str(AlgoAnalysis.insertion_sort_comparisons_count))
+            print("Amount of swap operations: " + str(AlgoAnalysis.insertion_sort_swaps_count))
+        elif 'heap' in algorithm_name:
+            print("Amount of comparison operations: " + str(AlgoAnalysis.heapsort_comparisons_count))
+            print("Amount of swap operations: " + str(AlgoAnalysis.heapsort_swaps_count))
