4b07bb1067cdf362f0e2124f22e48b87e33fdd29
aekrops <svyatoslavtrostynski24@gmail.com>
2020-11-28 01:03:54 +0200

    added validation for graph

--- lab3/graph.py
+++ lab3/graph.py
@@ -13,6 +13,8 @@ class Graph:
         return str(self.graph)
 
     def _add_edge_from_vertices(self, from_vertex: int, to_vertex: int, weight: int):
+        if (type(from_vertex) is not int) or (type(to_vertex) is not int) or (type(weight) is not int):
+            raise TypeError("not int :-( ")
         if weight < 0:
             raise ValueError("Weight not able to be negative")
         if from_vertex not in self.graph.keys():
