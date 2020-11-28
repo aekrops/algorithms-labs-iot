37695cfb373a81a5cb832b6abd8ead90b4d1e620
aekrops <svyatoslavtrostynski24@gmail.com>
2020-11-28 04:41:04 +0200

    update methods for reading files

--- lab3/main.py
+++ lab3/main.py
@@ -1,25 +1,33 @@
-from lab3 import gamesrv
 from lab3.graph import Graph
-from lab3.gamesrv import dijkstra
+from lab3.gamesrv import min_max_latency
 
 
-# v1, v2 = input().split()
-def read_graph(file):
+def create_graph_by_file(file):
     graph = Graph()
     with open(file, 'r') as file:
-        all_data = file.readlines()
-        vertices, edges = all_data[0].split()
-        clients = map(int, all_data[1].split())
-        edges = all_data[2:]
+        data = file.readlines()
+        number_of_vertices, number_of_edges = [int(number) for number in data[0].split()]
+        clients = [int(number) for number in data[1].split()]
+        edges = [list(map(int, edge.split())) for edge in data[2:]]
         for edge in edges:
-            start_vertex, end_vertex, latency = int(edge[0]), int(edge[2]), int(edge[4])
-            graph.add_edge(start_vertex, end_vertex, latency)
-        for vertex in edges:
-            if vertex[0] not in clients:
-                # latency = dijkstra(graph, vertex)
-                # print(latency)
-                # print(vertex[0])
-                pass
-if __name__ == '__main__':
-    read_graph("data/gamsrv1.in")
+            graph.add_edge(edge[0], edge[1], edge[2])
+        return {"graph": graph, "clients": clients}
 
+
+def min_max_ping(file):
+    data = create_graph_by_file(file)
+    graph = data['graph']
+    clients = data['clients']
+    return min_max_latency(graph, clients)
+
+
+def read_result(file):
+    with open(file, 'r') as file:
+        result = int(file.readline())
+        return result
+
+
+def check_result(file_in, file_out):
+    data_in = min_max_ping(file_in)
+    data_out = read_result(file_out)
+    return data_in == data_out
