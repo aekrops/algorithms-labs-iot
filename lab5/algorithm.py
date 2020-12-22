050bfaa10286a523179be827533e157ba004e6c2
aekrops <svyatoslavtrostynski24@gmail.com>
2020-12-22 09:07:50 +0200

    added algorithm

---
+++ lab5/algorithm.py
@@ -0,0 +1,39 @@
+from lab5.constant import prime_number, size
+
+
+def rabin_karp(text: str, pattern: str):
+    found_patterns_idx = []
+    text_len = len(text)
+    pattern_len = len(pattern)
+    if text_len < pattern_len:
+        return -1
+    pattern_hash = hash(pattern, pattern_len)
+    text_hash = hash(text, pattern_len)
+    if pattern_hash == text_hash:
+        found_patterns_idx.append({'start': 0, 'end': text_len})
+    for i in range(1, text_len - pattern_len + 1):
+        text_hash = substring_new_hash(pattern, text_hash, text[i - 1], text[i + pattern_len - 1])
+        if pattern_hash == text_hash:
+            if check_match(text, pattern, i):
+                found_patterns_idx.append({'start': i, 'end': i + pattern_len})
+    return found_patterns_idx
+
+
+def hash(pattern, pattern_len):
+    pattern_hash = 0
+    for i in range(pattern_len):
+        pattern_hash = (pattern_hash * size + ord(pattern[i])) % prime_number
+    return pattern_hash
+
+
+# hash for  substring with next letter
+def substring_new_hash(pattern_length, prev_hash, old_letter, new_letter):
+    new_hash = size ** (len(pattern_length) - 1) % prime_number
+    return ((prev_hash - ord(old_letter) * new_hash) * size + ord(new_letter)) % prime_number
+
+
+def check_match(text, pattern, start_index):
+    for i in range(len(pattern)):
+        if pattern[i] != text[start_index + i]:
+            return False
+    return True
---
+++ lab5/constant.py
@@ -0,0 +1,3 @@
+prime_number = 1035007
+
+size = 256  # size of ascii table
